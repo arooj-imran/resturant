2f1c14e28fbdebe5a0979ccf8c68b325fd3cccde
arooj-imran <urooj2539@gmail.com>
2024-10-01 16:21:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert("Login")
+// window.location.href="log.html"
+function orderNow(){
+    cartCount++;
+    document.getElementById("cart-count").innerText=cartCount;
+} 
+
+let cartCount=0;
+const menu=document.getElementById("menues");
+ menues.foreach(menue=>{
+    const menuesDiv=document.createElement("div");
+ menus.appendchild(menuesDiv);
+})
+  
